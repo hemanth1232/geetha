de519d6ca77b6b13b687172af5704328c376908b
hemanth1232 <68787928+hemanth1232@users.noreply.github.com>
2020-07-29 21:40:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let slideIndex = 0;
+
+function showSlides() {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    console.log(slides[2].style.display);
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2500);
+}
+showSlides()
+
