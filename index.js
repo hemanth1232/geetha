1157789bf242d68ec3807681ba7c73baf7403489
hemanth1232 <68787928+hemanth1232@users.noreply.github.com>
2020-07-30 07:38:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 1500);
+    setTimeout(showSlides, 2500);
 }
-showSlides()
+setTimeout(showSlides, 1500);
 
